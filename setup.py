cb40366d8f061eacab72fd45b88ae9d4b5da4416
ronfe <chihungfei@126.com>
2016-07-05 15:26:39 +0800

    [ronfe] update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pasta',
-      version='0.1',
+     version='0.2',
       description='The Yangcong Data Requirement ToolKit',
       url='http://github.com/guanghetv/pasta',
       author='ronfe',
